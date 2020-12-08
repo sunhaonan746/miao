95ed019cdbf44887b4dd7c7d9eba199fc0b20270
sunhaonan <1054203250@qq.com>
2020-12-08 20:10:00 +0800

    every

--- lodash/sunhaonan746-lodash.js
+++ lodash/sunhaonan746-lodash.js
@@ -79,13 +79,11 @@ var sunhaonan746 = function () {
         for (var i = 0; i < array.length;i++){
             if (typeof (array[i]) == "object") {
                 sum = array[i]
-                array.splice(i)
+                array.splice(i,1,...array[i])
                 break
             }
         } 
-        for (var i = 0; i < sum.length;i++){
-            array.push(sum[i])
-        }
+        
         return array
     }
 
@@ -104,6 +102,102 @@ var sunhaonan746 = function () {
     }
 
 
+    //与_.toPairs正好相反；这个方法返回一个由键值对pairs构成的对象。
+    function fromPairs(pairs) {
+        var map = {}
+        for (var i = 0; i < pairs.length;i++){
+            map[pairs[i][0]] = pairs[i][1]
+        }
+        return map
+    }
+
+
+
+    //获取数组 array 的第一个元素。
+    function head(array) {
+        return array[0]
+    }
+
+
+    //使用SameValueZero 等值比较，返回首次 value 在数组array中被找到的 索引值，
+    //如果 fromIndex 为负值，将从数组array尾端索引进行匹配。
+    function indexOf(array, value, fromIndex = 0) {
+        if (fromIndex < 0) {
+            return lastIndexOf(array,value)
+        } else {
+            for (var i = fromIndex; i < array.length;i++){
+                if (array[i] == value) {
+                    return i
+                }
+            }
+            return - 1
+        }
+    }
+
+    //获取数组array中除了最后一个元素之外的所有元素（注：去除数组array中的最后一个元素）。
+    function initial(array) {
+        array.length = array.length - 1
+        return array
+    }
+
+    //创建一个新数组，包含原数组中所有的非假值元素。例如false, null,0, "", undefined, 和 NaN 都是被认为是“假值”。
+    function compact(array) {
+        for (var i = 0; i < array.length;i++){
+            if (!array[i]) {
+                array.splice(i,1)
+            }
+        }
+        return array
+    }
+    
+
+    //创建一个切片数组，去除array前面的n个元素。（n默认值为1。）
+    function drop(array, n = 1) {
+        var result = []
+        for (var i = n; i < array.length;i++){
+            result.push(array[i])
+        }
+        return result
+    }
+
+
+
+    //创建一个切片数组，去除array尾部的n个元素。（n默认值为1。）
+    function dropRight(array, n = 1) {
+        var result = []
+        for (var i = 0; i < array.length -n;i++){
+            result.push(array[i])
+        }
+        return result
+    }
+
+
+    //反转array，使得第一个元素变为最后一个元素，第二个元素变为倒数第二个元素，依次类推。
+    function reverse(array) {
+        var result = []
+        for (var i = array.length -1; i >=0 ;i--){
+            result.push(array[i])
+        }
+        return result
+    }
+
+
+    //使用二进制的方式检索来决定 value值 应该插入到数组中 尽可能小的索引位置，以保证array的排序。
+    function sortedIndex(array, value) {
+        var a = true
+        for (var i = 0; i < array.length ;i++){
+            if (array[i] >= value) {
+                a =false
+                return i
+            }
+        }
+        if (a) {
+            return array.length
+        }
+    }
+    
+    
+    
     return {
         chunk,
         join,
@@ -114,5 +208,14 @@ var sunhaonan746 = function () {
         flatten,
         flattenDeep,
         flattenDepth,
+        fromPairs,
+        head,
+        indexOf,
+        initial,
+        compact,
+        drop,
+        dropRight,
+        reverse,
+        sortedIndex,
     }
 }()
