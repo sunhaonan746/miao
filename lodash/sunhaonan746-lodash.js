4c02350ed3253deaf6095b5dd58f85a66adbe88a
sunhaonan <1054203250@qq.com>
2020-12-08 17:28:41 +0800

    indexof

--- lodash/sunhaonan746-lodash.js
+++ lodash/sunhaonan746-lodash.js
@@ -16,7 +16,72 @@ var sunhaonan746 = function () {
         return result
     }
     
+
+    function join(array, val) {
+        var result =""
+        for (var i = 0; i < array.length; i++){
+            if (i < array.length -1) {
+                result+=array[i] + val
+            } else {
+                result+=array[i]
+            }
+        }
+        return result
+    }
+
+    function last(array) {
+        return array[array.length - 1]
+    }
+
+    function lastIndexOf(array, value, number = array.length -1) {
+        for (var i = number; i >= 0; i--){
+            if (array[i] == value) {
+                return i
+            }
+        }
+        return -1
+    }
+
+    function fill(array, value, start = 0, end = array.length) {
+        for (var i = start; i < end  ;i++){
+            array[i] = value
+        }
+        return array
+    }
+
+    function findIndex(array, predicate = _.identity, fromIndex = 0) {
+        for (var i = 0; i < array.length;i++){
+            if (predicate(array[i])) {
+                return i
+            }
+        }
+        return -1
+    }
+
+
+    function flatten(array) {
+        var sum 
+        for (var i = 0; i < array.length;i++){
+            if (typeof (array[i]) == "object") {
+                sum = array[i]
+                array.splice(i)
+                break
+            }
+        } 
+        for (var i = 0; i < sum.length;i++){
+            array.push(sum[i])
+        }
+        return array
+    }
+
+
     return {
         chunk,
+        join,
+        last,
+        lastIndexOf,
+        fill,
+        findIndex,
+        flatten,
     }
 }()
